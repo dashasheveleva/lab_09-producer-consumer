dd568bc80f2bdfaf6ed262fc653e1f613009e1a9
dashasheveleva <photodoshfy@gmail.com>
2022-05-27 04:36:11 -0700

    Create a new directory for load.cpp

--- demo1/load.cpp
+++ demo1/load.cpp
@@ -11,20 +11,20 @@ int loading_html() {
         "sync-ssl/http_client_sync_ssl.cpp"; //
     int version = 11;
 
-    // The io_context is required for all I/O
+    // io_context требуется для всех операций ввода/вывода
     boost::asio::io_context ioc;
 
-    // The SSL context is required, and holds certificates
+    // Требуется контекст SSL и содержит сертификаты
     ssl::context ctx{ ssl::context::sslv23_client };
 
-    // This holds the root certificate used for verification
+    // Здесь хранится корневой сертификат, используемый для проверки
     load_root_certificates(ctx);
 
-    // These objects perform our I/O
+    // Эти объекты выполняют наш ввод-вывод
     boost::asio::ip::tcp::resolver resolver{ ioc };
     ssl::stream<boost::asio::ip::tcp::socket> stream{ ioc, ctx };
 
-    // Set SNI Hostname (many hosts need this to handshake successfully)
+    // Установить имя хоста SNI (многим хостам это нужно для успешного рукопожатия)
     if (!SSL_set_tlsext_host_name(stream.native_handle(), host))
     {
       boost::system::error_code ec{ static_cast<int>(::ERR_get_error()),
@@ -32,17 +32,16 @@ int loading_html() {
       throw boost::system::system_error{ ec };
     }
 
-    // Look up the domain name
+    // Ищем доменное имя
     auto const results = resolver.resolve(host, port);
 
-    // Make the connection on the IP address we get from a lookup
-    boost::asio::connect(stream.next_layer(), results.begin(),
-                         results.end());
+    // Устанавливаем соединение по IP-адресу, полученному из поиска
+    boost::asio::connect(stream.next_layer(), results.begin(), results.end());
 
-    // Perform the SSL handshake
+    // Выполняем рукопожатие SSL
     stream.handshake(ssl::stream_base::client);
 
-    // Set up an HTTP GET request message
+    // Настройка сообщения запроса HTTP GET
     boost::beast::http::request<boost::beast::http::string_body> req{
         boost::beast::http::verb::get, target,
         version };
@@ -50,34 +49,26 @@ int loading_html() {
     req.set(boost::beast::http::field::user_agent,
             BOOST_BEAST_VERSION_STRING);
 
-    // Send the HTTP request to the remote host
+    // Отправляем HTTP-запрос на удаленный хост
     boost::beast::http::write(stream, req);
 
-    // This buffer is used for reading and must be persisted
+    // Этот буфер используется для чтения и должен быть сохранен
     boost::beast::flat_buffer buffer;
 
-    // Declare a container to hold the response
+    // Объявить контейнер для хранения ответа
     boost::beast::http::response<boost::beast::http::dynamic_body> res;
 
-    // Receive the HTTP response
+    // Получаем HTTP-ответ
     boost::beast::http::read(stream, buffer, res);
 
-    // Write the message to out
+    // Записываем сообщение на стандартный вывод
     std::ofstream ofs{"out.txt"}; // запись html-страницы в файл
     ofs << res;
     ofs.close();
 
-    // Gracefully close the stream
+    /// Изящно закрыть поток
     boost::system::error_code ec;
     stream.shutdown(ec);
-    if (ec == boost::asio::error::eof)
-    {
-      ec.assign(0, ec.category());
-    }
-    if (ec)
-      throw boost::system::system_error{ ec };
-
-    // If we get here then the connection is closed gracefully
   }
   catch (std::exception const& e)
   {
